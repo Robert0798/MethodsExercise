69dc148fbd0d335df1da70055c69d4c624f8bb48
Robert Anderson <rganderson0798@gmail.com>
2024-10-10 14:54:11 -0700

    Created Methods

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,9 +2,51 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+          GetUserNameAndGreet();
+          Console.WriteLine(Add(2,4));
+          Console.WriteLine(Subtract(2,4));
+          Console.WriteLine(Multiply(2,4));
+          Console.WriteLine(Divide(2,4));
+        }
+        public static void GetUserNameAndGreet()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+            Console.WriteLine("What is your favorite boba flavor?");
+            string boba = Console.ReadLine();
+            Console.WriteLine($"Hi, {name}! I love the band {band} as well! And my favorite color is  {favoriteColor} too! We need to get {boba} soon!");
+            
+            
+            
+            
+        }
+
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        static int Divide(int a, int b)
+        {
+            return a / b;
         }
     }
+   
 }
